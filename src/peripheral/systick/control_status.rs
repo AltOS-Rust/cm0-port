a23c39e6fd96f498c8b4463a66b627a75ddea757
Daniel Seitz <dnseitz@gmail.com>
2017-04-09 22:01:46 -0700

    systick mod tests

--- src/peripheral/systick/control_status.rs
+++ src/peripheral/systick/control_status.rs
@@ -57,3 +57,63 @@ impl CSR {
         (self.0 & COUNTFLAG) != 0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_csr_set_enable_on() {
+        let mut csr = CSR(0);
+
+        csr.set_enable(true);
+        assert_eq!(csr.0, 0b1);
+    }
+
+    #[test]
+    fn test_csr_set_enable_off() {
+        let mut csr = CSR(0b1);
+
+        csr.set_enable(false);
+        assert_eq!(csr.0, 0);
+    }
+
+    #[test]
+    fn test_csr_set_interrupt_on() {
+        let mut csr = CSR(0);
+
+        csr.set_interrupt(true);
+        assert_eq!(csr.0, 0b1 << 1);
+    }
+
+    #[test]
+    fn test_csr_set_interrupt_off() {
+        let mut csr = CSR(0);
+
+        csr.set_interrupt(false);
+        assert_eq!(csr.0, 0);
+    }
+
+    #[test]
+    fn test_csr_set_source() {
+        let mut csr = CSR(0);
+
+        csr.set_source(ClockSource::Processor);
+        assert_eq!(csr.0, 0b1 << 2);
+    }
+
+    #[test]
+    fn test_csr_did_underflow_false_if_underflow_bit_not_set() {
+        let csr = CSR(0);
+
+        assert_eq!(csr.did_underflow(), false);
+    }
+
+    #[test]
+    fn test_csr_did_underflow_true_if_underflow_bit_set() {
+        // underflow bit set at start
+        let csr = CSR(0b1 << 16);
+
+        assert_eq!(csr.did_underflow(), true);
+    }
+}
